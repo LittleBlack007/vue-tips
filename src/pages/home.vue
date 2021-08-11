8571318bc5a91512e8cf01b8101f5bde623c2c57
yuping.pang <pangyuping@linshimuye.com>
2021-08-11 18:19:29 +0800

    antd配置按需加载

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -2,16 +2,19 @@
   <div>
     <div><img src="../assets/logo.png"></div>
     <EventBus />
+    <a-button type="primary">ant button</a-button>
   </div>
 </template>
 
 <script>
 import EventBus from './event-bus.vue';
+import {Button} from 'ant-design-vue';
 
 export default {
   name: 'Home',
   components:{
-    EventBus
+    EventBus,
+    AButton: Button
   },
   data(){
     return {
